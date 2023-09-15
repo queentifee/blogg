2da95abbef68fb430ac9f5f894fd36eb1ba2b2d4
queenie <samuellqueenie@gmail.com>
2023-09-15 10:23:17 +0100

    Updated settings in mysite/settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles','blog',
+    'django.contrib.staticfiles','blog', 'mysite'
 ]
 
 MIDDLEWARE = [
